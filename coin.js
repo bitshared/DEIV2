dfc7be5d8d5d963b6a0834983fe4af9a69559905
BitShared <55608004+bitshared@users.noreply.github.com>
2024-12-24 17:43:20 +0700

    update coin.js

---
+++ coin.js
@@ -0,0 +1,16 @@
+const vcpu = {
+    ch: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    enc: function (mz) {
+        let ec = (mz + "=").replace(/-/g, '+').replace(/_/g, '/').split('').
+            map(i => this.ch.indexOf(i).toString(2).padStart(6, 0)).join("").
+            match(/.{8}/g).map(i => parseInt(i, 2).toString(16).padStart(2, 0)).join("");
+        return ec
+    },
+    dec: function (dz) {
+        let dc = dz.match(/.{2}/g).map(i => parseInt(i, 16).toString(2).padStart(8, 0)).join('').
+            match(/.{6}/g).map(i => parseInt(i, 2)).
+            map(i => this.ch[i]).
+            join("").replace("+", '-').replace("/", '_')
+        return dc
+    }
+}
